f2d7d506c899fe33e7d0ade032a9971d848c8389
H.K <16552133+hdk2200@users.noreply.github.com>
2024-05-17 11:42:05 +0900

    TBD:

--- KeychainExample/KeychainValue.swift
+++ KeychainExample/KeychainValue.swift
@@ -1,5 +1,5 @@
-
 import Foundation
+import Security
 
 enum KeychainValueError: Error {
   case dataConvert
@@ -8,16 +8,16 @@ enum KeychainValueError: Error {
 
 public class KeychainValue: KeychainValueProtocol {
   private let service: String
-  
+    
   public init(service: String = Bundle.main.bundleIdentifier ?? "defaultService") {
     self.service = service
   }
-  
-  private func makeUnexpectedError(status: OSStatus) -> KeychainValueError {
+    
+  private static func makeUnexpectedError(status: OSStatus) -> KeychainValueError {
     let desc = SecCopyErrorMessageString(status, nil) as String? ?? "Unknown keychain error"
     return .unexpectedError(status: status, description: desc)
   }
-  
+    
   public func get(_ key: String) throws -> String? {
     let query: [String: Any] = [
       kSecClass as String: kSecClassGenericPassword,
@@ -26,7 +26,7 @@ public class KeychainValue: KeychainValueProtocol {
       kSecMatchLimit as String: kSecMatchLimitOne,
       kSecReturnData as String: kCFBooleanTrue!
     ]
-    
+        
     var result: AnyObject?
     let status = SecItemCopyMatching(query as CFDictionary, &result)
         
@@ -34,7 +34,7 @@ public class KeychainValue: KeychainValueProtocol {
       if status == errSecItemNotFound {
         return nil
       } else {
-        throw makeUnexpectedError(status: status)
+        throw KeychainValue.makeUnexpectedError(status: status)
       }
     }
         
@@ -49,35 +49,34 @@ public class KeychainValue: KeychainValueProtocol {
     guard let data = value.data(using: .utf8) else {
       throw KeychainValueError.dataConvert
     }
-      
+        
     var query: [String: Any] = [
       kSecClass as String: kSecClassGenericPassword,
       kSecAttrService as String: service,
       kSecAttrAccount as String: key
     ]
-    
+        
     let status = SecItemCopyMatching(query as CFDictionary, nil)
+        
     if status == errSecItemNotFound {
       // Add new item
-      query[kSecValueData as String] = value.data(using: .utf8)!
+      query[kSecValueData as String] = data
       let addStatus = SecItemAdd(query as CFDictionary, nil)
       guard addStatus == errSecSuccess else {
-        throw makeUnexpectedError(status: addStatus)
+        throw KeychainValue.makeUnexpectedError(status: addStatus)
       }
-    }
-    else if status == errSecSuccess {
+    } else if status == errSecSuccess {
       // Update existing item
-      let attributes: [String: Any] = [kSecValueData as String: value.data(using: .utf8)!]
+      let attributes: [String: Any] = [kSecValueData as String: data]
       let updateStatus = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
       guard updateStatus == errSecSuccess else {
-        throw makeUnexpectedError(status: updateStatus)
+        throw KeychainValue.makeUnexpectedError(status: updateStatus)
       }
-    }
-    else {
-      throw makeUnexpectedError(status: status)
+    } else {
+      throw KeychainValue.makeUnexpectedError(status: status)
     }
   }
-  
+    
   public func remove(_ key: String) throws {
     let query: [String: Any] = [
       kSecClass as String: kSecClassGenericPassword,
@@ -87,7 +86,7 @@ public class KeychainValue: KeychainValueProtocol {
         
     let status = SecItemDelete(query as CFDictionary)
     guard status == errSecSuccess || status == errSecItemNotFound else {
-      throw makeUnexpectedError(status: status)
+      throw KeychainValue.makeUnexpectedError(status: status)
     }
   }
     
@@ -99,7 +98,7 @@ public class KeychainValue: KeychainValueProtocol {
         
     let status = SecItemDelete(query as CFDictionary)
     guard status == errSecSuccess || status == errSecItemNotFound else {
-      throw makeUnexpectedError(status: status)
+      throw KeychainValue.makeUnexpectedError(status: status)
     }
   }
 }
