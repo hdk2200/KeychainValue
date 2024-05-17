3a2744e22d6eeea7571491d947177450dc9ab3eb
H.K <16552133+hdk2200@users.noreply.github.com>
2024-05-17 14:56:40 +0900

    Format code.

--- KeychainExample/KeychainExampleApp.swift
+++ KeychainExample/KeychainExampleApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct KeychainExampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
